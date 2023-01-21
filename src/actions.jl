a989c320bfb7930969be31005394619c105c6fe8
Lorenzo Bonanni <lorenzo.bonanni@studenti.univr.it>
2023-01-21 13:20:14 +0100

    Added missing commas, modified ACTION_DIRS
    Added missing commas, modified ACTION_DIRS according to BASIC_ACTIONS_DICT

--- src/actions.jl
+++ src/actions.jl
@@ -1,15 +1,15 @@
 const N_BASIC_ACTIONS = 5
-const BASIC_ACTIONS_DICT = Dict(:sample => 1
+const BASIC_ACTIONS_DICT = Dict(:sample => 1,
                                 :north => 2, 
                                 :east => 3,
                                 :south => 4,
                                 :west => 5)
 
-const ACTION_DIRS = (RSPos(0,1),
+const ACTION_DIRS = (RSPos(0,0),
+                    RSPos(0,1),
                     RSPos(1,0),
                     RSPos(0,-1),
-                    RSPos(-1,0),
-                    RSPos(0,0))
+                    RSPos(-1,0))
 
 POMDPs.actions(pomdp::RockSamplePOMDP{K}) where K = 1:N_BASIC_ACTIONS+K
 POMDPs.actionindex(pomdp::RockSamplePOMDP, a::Int) = a
